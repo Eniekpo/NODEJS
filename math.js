ba73a393980b2a70bc48dd9c2c8bc35ff2817fe9
Eniekpo <johneniekpo4life@gmail.com>
2023-02-20 11:44:30 +0100

    a

---
+++ math.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const mod = (a, b) => a % b;
+
+
+module.exports = {add, multiply, divide, subtract, mod}
---
+++ server.js
@@ -0,0 +1,21 @@
+console.log("Hello world!");
+// console.log(global);
+
+const os = require("os");
+const path = require("path");
+const {add, subtract, multiply, divide, mod} = require("./math");
+
+// console.log('OS Type: ', os.type());
+// console.log('OS Version: ', os.version());
+// console.log('OS Home Dir ', os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__dirname));
+
+console.log(add(9, 2));
+console.log(subtract(9, 2));
+console.log(multiply(9, 2));
+console.log(divide(9, 2));
+console.log(mod(9, 2));
